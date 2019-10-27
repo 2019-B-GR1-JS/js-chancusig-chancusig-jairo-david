57b533887ded8481e159f4f29242b87f2775a154
JairockaxD <jairocka00@hotmail.com>
2019-10-27 15:23:17 -0500

    Deber - Operadores de forma Local

--- 01 - Javascript/05-callbacks.js
+++ 01 - Javascript/05-callbacks.js
@@ -26,6 +26,10 @@ const resultadoCalculo = calculo(
 console.log(resultadoCalculo);
 
 
+/////////////////////////////////////////////////////////////
+////////////////DEBER OPERADORES////////////////////////////
+
+///forEach
 function forEachLocal(arreglo, funcion) {
        for(let i = 0;i<arreglo.length;i ++){
         funcion(arreglo[i],i,arreglo);
@@ -38,25 +42,129 @@ forEachLocal([1,2,3,4,5],
     console.log('arreglo', arreglo);
     }
 );
-function mapLocal(arreglo, funcion, ) {
-    for(let i = 0;i<arreglo.length;i ++){
-        funcion(arreglo[i],i,arreglo);
 
+///Map
+function mapLocal(arreglo,funcion){
+    const nuevoArreglo = [];
+    for (let i =0; i<arreglo.length; i ++){
+        const nuevoValor = funcion(arreglo[i]);
+        nuevoArreglo.push(nuevoValor)
     }
+    console.log(nuevoArreglo);
 }
- mapLocal([1,2,3,4,5,], (valorActual, indiceActual, arreglo)=> {
-    console.log('valorActual', valorActual);
-    console.log('indiceActual', indiceActual);
-    console.log('arreglo', arreglo);
-})
 
-const arreglo1 = [1,2,3,4,5];
-const respuestaMap = arreglo1;
+mapLocal([1,2,3,4,5],(valorActual)=>{
+    return valorActual + 5;
+});
 
-.map(
-    function (indiceActual, valorActual, arreglo1 ) {
-        return valorActual + 5;
+
+///Filter
+function filterLocal(arreglo,funcion){
+    const nuevoArreglo = [];
+    for (let i =0; i<arreglo.length; i++){
+        let nuevoValor = funcion(arreglo[i]);
+
+        if (nuevoValor === true ) {
+            nuevoArreglo.push(arreglo[i]);
+        }
     }
-);
-    console.log('respuestaMap', respuestaMap);
+    console.log(nuevoArreglo);
+}
+
+filterLocal([1,2,3,4,5],(valorActual)=>{
+    return valorActual + 5 >= 10;
+});
+
+///Reduce
+function reduceLocal(arreglo,funcion){
+    //let respuestaReduce = undefined;
+    let nuevoValor = 100;
+    for (let i =0; i < arreglo.length; i ++){
+        nuevoValor = funcion(nuevoValor, arreglo[i]);
+    }
+    console.log('Resultado:', nuevoValor);
+}
+
+reduceLocal([1,2,3,4,5],(valorInicial,valorActual)=>{
+    return valorInicial - valorActual;
+});
+
+///Some
+function someLocal(arreglo,funcion){
+    let some = false;
+    for (let i =0; i<arreglo.length; i ++){
+        const nuevoValor = funcion(arreglo[i]);
+
+        if(nuevoValor === true ){
+            some = true;
+        }
+    }
+    console.log('Resultado:', some);
+}
+
+someLocal([1,2,3,4,5],(valorActual)=>{
+    return valorActual > 2 && valorActual < 5;
+});
+
+
+///Every
+function everyLocal(arreglo,funcion){
+    let every = true;
+    for (let i =0; i<arreglo.length; i ++){
+        const nuevoValor = funcion(arreglo[i]);
+        if(nuevoValor === false ){
+            every = false;
+        }
+    }
+    console.log('Resultado:', every);
+}
+
+everyLocal([1,2,3,4,5],(valorActual)=>{
+    return valorActual > 2 && valorActual < 5;
+});
+
+
+///Find
+function findLocal(arreglo,funcion){
+    let find = undefined;
+    let i = 0;
+    while(i<arreglo.length){
+        const nuevoValor = funcion(arreglo[i]);
+        if(typeof nuevoValor === "number"){
+            find = nuevoValor;
+            break;
+        }
+        i++;
+    }
+    console.log('Resultado:', find);
+}
+
+findLocal([1,2,3,4,5],(valorActual)=>{
+    if (valorActual < 5){
+        return valorActual;
+    }
+});
+
+
+///Find Index
+function findIndexLocal(arreglo,funcion){
+    let findIndex = undefined;
+    let i = 0;
+    while(i < arreglo.length){
+        const nuevoValor = funcion(arreglo[i],i);
+        if(typeof nuevoValor === "number"){
+            findIndex = nuevoValor;
+            break;
+        }
+        i++;
+    }
+    console.log('Resultado:', findIndex);
+}
+
+findIndexLocal([1,2,3,4,5],(valorActual,indiceActual)=>{
+    if (valorActual > 2){
+        return indiceActual;
+    }
+});
+
 
