5d8088c36c8ee0c296412fc051056806a3da3482
JairockaxD <jairocka00@hotmail.com>
2020-01-05 13:00:37 -0500

    validaciones en formularios

---
+++ 04-ionic/comida/src/app/tab3/tab3.page.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+    usuario = {
+        nombre: '',
+        correo: '',
+        edad: 0,
+        password: '',
+        passwordConfirmacion: ''
+    };
+
+    constructor() {
+    }
+
+    crearUsuario(formulario: NgForm) {
+        console.log(formulario);
+    }
+
+    validarPasswordsIguales(): boolean {
+        if (this.usuario.password === this.usuario.passwordConfirmacion) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
